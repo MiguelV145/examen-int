1be94b11bc6790883ad4485023d2bd4a3b5ce026
miguelv145 <miguelvanegas145@gmail.com>
2025-12-06 18:58:47 -0500

    first commit

---
+++ src/app/features/Component/Footer/Footer.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  templateUrl: './Footer.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Footer { 
+
+ currentYear: number = new Date().getFullYear();
+}
